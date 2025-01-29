6d0363a46b85f970a7a74d1854de6e5d5b8113ea
Lone-Suhail <lonesuhail02@gmail.com>
2025-01-29 22:52:27 +0530

    Update index.jsx

--- gotrip/components/header/default-header/index.jsx
+++ gotrip/components/header/default-header/index.jsx
@@ -1,12 +1,12 @@
 
-'use client'
+'use client';
 
 import Link from "next/link";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import MainMenu from "../MainMenu";
 import CurrenctyMegaMenu from "../CurrenctyMegaMenu";
 import LanguageMegaMenu from "../LanguageMegaMenu";
-
 import MobileMenu from "../MobileMenu";
 
 const Header1 = () => {
@@ -28,101 +28,103 @@ const Header1 = () => {
   }, []);
 
   return (
-    <>
-      <header className={`header bg-white ${navbar ? "is-sticky" : ""}`}>
-        <div className="header__container px-30 sm:px-20">
-          <div className="row justify-between items-center">
-            <div className="col-auto">
-              <div className="d-flex items-center">
-                <Link href="/" className="header-logo mr-20">
-                  <img src="/img/general/logo-light.svg" alt="logo icon" />
-                  <img src="/img/general/logo-light.svg" alt="logo icon" />
-                </Link>
-                {/* End logo */}
+    <header className={`header bg-white ${navbar ? "is-sticky" : ""}`}>
+      <div className="header__container px-30 sm:px-20">
+        <div className="row justify-between items-center">
+          <div className="col-auto">
+            <div className="d-flex items-center">
+              <Link href="/" className="header-logo mr-20">
+                <Image
+                  src="/img/general/logo-dark.svg"
+                  alt="My Website Logo"
+                  width={120}
+                  height={40}
+                />
+              </Link>
+              {/* End logo */}
 
-                <div className="header-menu">
-                  <div className="header-menu__content">
-                    <MainMenu style="text-dark-1" />
-                  </div>
+              <nav className="header-menu">
+                <div className="header-menu__content">
+                  <MainMenu style="text-dark-1" />
                 </div>
-                {/* End header-menu */}
-              </div>
-              {/* End d-flex */}
+              </nav>
+              {/* End header-menu */}
             </div>
-            {/* End col */}
+            {/* End d-flex */}
+          </div>
+          {/* End col */}
 
-            <div className="col-auto">
-              <div className="d-flex items-center">
-                <div className="row x-gap-20 items-center xxl:d-none">
-                  <CurrenctyMegaMenu textClass="text-dark-1" />
-                  {/* End Megamenu for Currencty */}
+          <div className="col-auto">
+            <div className="d-flex items-center">
+              <div className="row x-gap-20 items-center xxl:d-none">
+                <CurrenctyMegaMenu textClass="text-dark-1" />
+                {/* End Megamenu for Currencty */}
 
-                  {/* Start vertical devider*/}
-                  <div className="col-auto">
-                    <div className="w-1 h-20 bg-white-20" />
-                  </div>
-                  {/* End vertical devider*/}
-
-                  <LanguageMegaMenu textClass="text-dark-1" />
-                  {/* End Megamenu for Language */}
+                {/* Start vertical divider */}
+                <div className="col-auto">
+                  <div className="w-1 h-20 bg-white-20" />
                 </div>
-                {/* End language and currency selector */}
+                {/* End vertical divider */}
 
-                {/* Start btn-group */}
-                <div className="d-flex items-center ml-20 is-menu-opened-hide md:d-none">
+                <LanguageMegaMenu textClass="text-dark-1" />
+                {/* End Megamenu for Language */}
+              </div>
+              {/* End language and currency selector */}
+
+              {/* Start btn-group */}
+              <div className="d-flex items-center ml-20 is-menu-opened-hide md:d-none">
+                <Link
+                  href="/login"
+                  className="button px-30 fw-400 text-14 -blue-1 bg-blue-1 h-50 text-white"
+                >
+                  Become An Expert
+                </Link>
+                <Link
+                  href="/signup"
+                  className="button px-30 fw-400 text-14 -outline-blue-1 h-50 text-blue-1 ml-20"
+                >
+                  Sign In / Register
+                </Link>
+              </div>
+              {/* End btn-group */}
+
+              {/* Start mobile menu icon */}
+              <div className="d-none xl:d-flex x-gap-20 items-center pl-30 text-dark-1">
+                <div>
                   <Link
                     href="/login"
-                    className="button px-30 fw-400 text-14 -blue-1 bg-blue-1 h-50 text-white"
-                  >
-                    Become An Expert
-                  </Link>
-                  <Link
-                    href="/signup"
-                    className="button px-30 fw-400 text-14 -outline-blue-1 h-50 text-blue-1 ml-20"
-                  >
-                    Sign In / Register
-                  </Link>
+                    className="d-flex items-center icon-user text-inherit text-22"
+                  />
                 </div>
-                {/* End btn-group */}
+                <div>
+                  <button
+                    className="d-flex items-center icon-menu text-inherit text-20"
+                    data-bs-toggle="offcanvas"
+                    aria-controls="mobile-sidebar_menu"
+                    data-bs-target="#mobile-sidebar_menu"
+                  />
 
-                {/* Start mobile menu icon */}
-                <div className="d-none xl:d-flex x-gap-20 items-center pl-30 text-dark-1">
-                  <div>
-                    <Link
-                      href="/login"
-                      className="d-flex items-center icon-user text-inherit text-22"
-                    />
-                  </div>
-                  <div>
-                    <button
-                      className="d-flex items-center icon-menu text-inherit text-20"
-                      data-bs-toggle="offcanvas"
-                      aria-controls="mobile-sidebar_menu"
-                      data-bs-target="#mobile-sidebar_menu"
-                    />
-
-                    <div
-                      className="offcanvas offcanvas-start  mobile_menu-contnet"
-                      tabIndex="-1"
-                      id="mobile-sidebar_menu"
-                      aria-labelledby="offcanvasMenuLabel"
-                      data-bs-scroll="true"
-                    >
-                      <MobileMenu />
-                      {/* End MobileMenu */}
-                    </div>
+                  <div
+                    className="offcanvas offcanvas-start mobile_menu-contnet"
+                    tabIndex="-1"
+                    id="mobile-sidebar_menu"
+                    aria-labelledby="offcanvasMenuLabel"
+                    data-bs-scroll="true"
+                  >
+                    <MobileMenu />
+                    {/* End MobileMenu */}
                   </div>
                 </div>
-                {/* End mobile menu icon */}
               </div>
+              {/* End mobile menu icon */}
             </div>
-            {/* End col-auto */}
           </div>
-          {/* End .row */}
+          {/* End col-auto */}
         </div>
-        {/* End header_container */}
-      </header>
-    </>
+        {/* End .row */}
+      </div>
+      {/* End header_container */}
+    </header>
   );
 };
 
