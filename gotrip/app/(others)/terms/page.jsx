a0b9708db279b261469a0bcac79183d1c9e55282
Lone-Suhail <lonesuhail02@gmail.com>
2025-01-31 11:24:52 +0530

    Update page.jsx

--- gotrip/app/(others)/terms/page.jsx
+++ gotrip/app/(others)/terms/page.jsx
@@ -5,8 +5,8 @@ import DefaultFooter from "@/components/footer/default";
 import TermsConent from "@/components/common/TermsConent";
 
 export const metadata = {
-  title: "Terms & Conditions || GoTrip - Travel & Tour React NextJS Template",
-  description: "GoTrip - Travel & Tour React NextJS Template",
+  title: "Terms & Conditions || Dream Trip - Travel & Tour React NextJS Template",
+  description: "Dream Trip - Travel & Tour React NextJS Template",
 };
 
 const Terms = () => {
