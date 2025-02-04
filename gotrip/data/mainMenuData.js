aaf1ef777e3ac65ccff5a607152774cc437f29ca
Lone-Suhail <lonesuhail02@gmail.com>
2025-02-05 00:19:43 +0530

    Update mainMenuData.js

--- gotrip/data/mainMenuData.js
+++ gotrip/data/mainMenuData.js
@@ -3,12 +3,104 @@ export const homeItems = [
     name: "Home",
     routePath: "/",
   },
-  // Uncomment if needed
-  // {
-  //   name: "Home 02",
-  //   routePath: "/home_2",
-  // },
+  {
+  //  name: "Home 02",
+ //   routePath: "/home_2",
+  },
 ];
+
+/*export const blogItems = [
+  {
+    name: "Blog List V1",
+    routePath: "/blog-list-v1",
+  },
+  {
+    name: "Blog List V2",
+    routePath: "/blog-list-v2",
+  },
+  {
+    name: "Blog Single",
+    routePath: "/blog-details/1",
+  },
+];
+
+export const pageItems = [
+  {
+    name: "404",
+    routePath: "/404",
+  },
+  {
+    name: "About",
+    routePath: "/about",
+  },
+  {
+    name: "Become Expert",
+    routePath: "/become-expert",
+  },
+  {
+    name: "Help Center",
+    routePath: "/help-center",
+  },
+  {
+    name: "Login",
+    routePath: "/login",
+  },
+  {
+    name: "Register",
+    routePath: "/signup",
+  },
+  {
+    name: "Terms",
+    routePath: "/terms",
+  },
+  {
+    name: "Invoice",
+    routePath: "/invoice",
+  },
+];
+
+export const dashboardItems = [
+  {
+    name: "Dashboard",
+    routePath: "/dashboard/db-dashboard",
+  },
+  {
+    name: "Booking History",
+    routePath: "/dashboard/db-booking",
+  },
+  {
+    name: "Wishlist",
+    routePath: "/dashboard/db-wishlist",
+  },
+  {
+    name: "Settings",
+    routePath: "/dashboard/db-settings",
+  },
+  {
+    name: "Vendor Dashboard",
+    routePath: "/vendor-dashboard/dashboard",
+  },
+  {
+    name: "Vendor Add Hotel",
+    routePath: "/vendor-dashboard/add-hotel",
+  },
+  {
+    name: "Vendor Booking",
+    routePath: "/vendor-dashboard/booking",
+  },
+  {
+    name: "Vendor Hotels",
+    routePath: "/vendor-dashboard/hotels",
+  },
+  {
+    name: "Vendor Recovery",
+    routePath: "/vendor-dashboard/recovery",
+  },
+  {
+    name: "Logout",
+    routePath: "/login",
+  },
+];*/
 
 export const categorieMegaMenuItems = [
   {
@@ -43,11 +135,10 @@ export const categorieMegaMenuItems = [
                 name: "Tour Map",
                 routePath: "/tour-list-v3",
               },
-              // Uncomment if needed
-              // {
-              //   name: "Tour Single",
-              //   routePath: "/tour-single/5",
-              // },
+              {
+                name: "Tour Single",
+                routePath: "/tour-single/5",
+              },
             ],
           },
         ],
@@ -83,13 +174,166 @@ export const categorieMobileItems = [
             name: "Tour Map",
             routePath: "/tour-list-v3",
           },
-          // Uncomment if needed
-          // {
-          //   name: "Tour Single",
-          //   routePath: "/tour-single/5",
-          // },
+         {
+        //    name: "Tour Single",
+         //   routePath: "/tour-single/5",
+          },
         ],
       },
     ],
   },
 ];
+ /*export const categorieMobileItems = [
+  {
+    id: 3,
+    title: "Activity",
+    menuItems: [
+      {
+        id: 1,
+        title: "Activity List",
+        menuList: [
+          {
+            name: "Activity List v1",
+            routePath: "/activity-list-v1",
+          },
+          {
+            name: "Activity List v2",
+            routePath: "/activity-list-v2",
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "Activity Pages",
+        menuList: [
+          {
+            name: "Activity Map",
+            routePath: "/activity-list-v3",
+          },
+          {
+            name: "Activity Single",
+            routePath: "/activity-single/3",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Hotel Rentals",
+    menuItems: [
+      {
+        id: 1,
+        title: "Rental List",
+        menuList: [
+          {
+            name: "Rental List v1",
+            routePath: "/rental-list-v1",
+          },
+          {
+            name: "Rental List v2",
+            routePath: "/rental-list-v2",
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "Rental Pages",
+        menuList: [
+          {
+            name: "Rental Map",
+            routePath: "/rental-list-v3",
+          },
+          {
+            name: "Rental Single",
+            routePath: "/rental-single/3",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Car",
+    menuItems: [
+      {
+        id: 1,
+        title: "Car List",
+        menuList: [
+          {
+            name: "Car List v1",
+            routePath: "/car-list-v1",
+          },
+          {
+            name: "Car List v2",
+            routePath: "/car-list-v2",
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "Car Pages",
+        menuList: [
+          {
+            name: "Car Map",
+            routePath: "/car-list-v3",
+          },
+          {
+            name: "Car Single",
+            routePath: "/car-single/1",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Cruise",
+    menuItems: [
+      {
+        id: 1,
+        title: "Cruise List",
+        menuList: [
+          {
+            name: "Cruise List v1",
+            routePath: "/cruise-list-v1",
+          },
+          {
+            name: "Cruise List v2",
+            routePath: "/cruise-list-v2",
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "Cruise Pages",
+        menuList: [
+          {
+            name: "Cruise Map",
+            routePath: "/cruise-list-v3",
+          },
+          {
+            name: "Cruise Single",
+            routePath: "/cruise-single/3",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: "Flights",
+    menuItems: [
+      {
+        id: 1,
+        title: "Flight List",
+        menuList: [
+          {
+            name: "Flight List v1",
+            routePath: "/flight-list-v1",
+          },
+        ],
+      },
+    ],
+  },
+];*/
