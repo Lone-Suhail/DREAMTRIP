00bbe02f9e680305f667296e496b2ee57a546e84
Lone-Suhail <lonesuhail02@gmail.com>
2025-02-07 00:54:17 +0530

    Update page.jsx

--- gotrip/app/(homes)/home_1/page.jsx
+++ gotrip/app/(homes)/home_1/page.jsx
@@ -123,23 +123,7 @@ const Home_1 = () => {
       </section>
       {/* End testimonial Section */}
 
-      <section className="layout-pt-lg layout-pb-md">
-        <div className="container">
-          <div className="row justify-center text-center">
-            <div className="col-auto">
-              <div className="sectionTitle -md">
-                <h2 className="sectionTitle__title">
-                  Get inspiration for your next trip
-                </h2>
-                <p className="sectionTitle__text mt-5 sm:mt-0">
-                  Interdum et malesuada fames
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* End .row */}
-          <div className="row y-gap-30 pt-40">
-         <section className="layout-pt-md layout-pb-lg">
+      <section className="layout-pt-md layout-pb-lg">
         <div className="container">
           <div className="row">
             <div className="col-auto">
