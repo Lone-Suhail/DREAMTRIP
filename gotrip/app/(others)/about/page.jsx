1340354a963bf7ddf120e653d63a9878b4317a14
Lone-Suhail <lonesuhail02@gmail.com>
2025-02-06 22:42:35 +0530

    Update page.jsx

--- gotrip/app/(others)/about/page.jsx
+++ gotrip/app/(others)/about/page.jsx
@@ -12,8 +12,8 @@ import Counter2 from "@/components/counter/Counter2";
 import Brand from "@/components/brand/Brand";
 
 export const metadata = {
-  title: "About || GoTrip - Travel & Tour React NextJS Template",
-  description: "GoTrip - Travel & Tour React NextJS Template",
+  title: "About || Dream Trip - Travel & Tour",
+  description: "Dream Trip - Travel & Tour",
 };
 
 const About = () => {
