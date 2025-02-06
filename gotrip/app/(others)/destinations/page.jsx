1a6d8e933d663eedf48136a3e8c5229c1b26622b
Lone-Suhail <lonesuhail02@gmail.com>
2025-02-07 01:02:55 +0530

    Update page.jsx

--- gotrip/app/(others)/destinations/page.jsx
+++ gotrip/app/(others)/destinations/page.jsx
@@ -109,28 +109,6 @@ const Destinations = () => {
             </div>
           </div>
           {/* End .row */}
-
-          <div className="row y-gap-30 pt-40 sm:pt-20 item_gap-x30">
-            <Hotels />
-          </div>
-          {/* End relative */}
-        </div>
-      </section>
-      {/* End  Hotel sections */}
-
-      <section className="layout-pt-md layout-pb-md">
-        <div className="container">
-          <div className="row y-gap-20 justify-between items-end">
-            <div className="col-auto">
-              <div className="sectionTitle -md">
-                <h2 className="sectionTitle__title">Most Popular Tours</h2>
-                <p className=" sectionTitle__text mt-5 sm:mt-0">
-                  Interdum et malesuada fames ac ante ipsum
-                </p>
-              </div>
-            </div>
-            {/* End .col */}
-
             <div className="col-auto">
               <Link
                 href="#"
