d27b2881af6197d13f9b8880d6a04ffaca9981bc
Lone-Suhail <lonesuhail02@gmail.com>
2025-02-07 23:36:33 +0530

    Update page.jsx

--- gotrip/app/(others)/destinations/page.jsx
+++ gotrip/app/(others)/destinations/page.jsx
@@ -15,7 +15,6 @@ import IntroTown from "@/components/destinations/components/IntroTown";
 import Weather from "@/components/destinations/components/Weather";
 import GeneralInfo from "@/components/destinations/components/GeneralInfo";
 
-
 export const metadata = {
   title: "Destinations || Dream Trip - Travel & Tour",
   description: "Dream Trip - Travel & Tour",
@@ -24,130 +23,76 @@ export const metadata = {
 const Destinations = () => {
   return (
     <>
-      {/* End Page Title */}
-
+      {/* Header */}
       <div className="header-margin"></div>
-      {/* header top margin */}
-
       <DefaultHeader />
-      {/* End Header 1 */}
-
       <LocationTopBar />
-      {/* End location top bar section */}
 
+      {/* Main Content */}
       <section className="layout-pb-md">
         <div className="container">
+          {/* Banner */}
           <div className="row">
             <Banner />
           </div>
-          {/* End .row */}
 
+          {/* Categories */}
           <div className="row x-gap-20 y-gap-20 items-center pt-20 item_gap-x10">
             <Categories />
           </div>
-          {/* End .row */}
 
+          {/* Introduction */}
           <div className="row y-gap-20 pt-40">
             <div className="col-auto">
               <h2>What to know before visiting Kashmir</h2>
             </div>
-            {/* End .col-auto */}
-
             <IntroTown />
           </div>
-          {/* End .row */}
 
+          {/* Separator */}
           <div className="pt-30 mt-30 border-top-light" />
-          {/* border separation */}
 
+          {/* Local Weather */}
           <div className="row y-gap-20">
             <div className="col-12">
               <h2 className="text-22 fw-500">Local weather</h2>
             </div>
-            {/* End. col-12 */}
-
             <Weather />
           </div>
-          {/* End local weather */}
 
+          {/* Separator */}
           <div className="pt-30 mt-30 border-top-light" />
+
+          {/* General Info */}
           <div className="row y-gap-20">
             <div className="col-12">
               <h2 className="text-22 fw-500">General info</h2>
             </div>
-            {/* End .col */}
             <GeneralInfo />
           </div>
-          {/* End .row */}
+
+          {/* Separator */}
           <div className="mt-30 border-top-light" />
-          {/* border separation */}
         </div>
-        {/* End .container */}
       </section>
-      {/* End Top Banner,categorie,intro,weather, generic info section */}
 
-      <section className="layout-pt-md layout-pb-md">
-        <div className="container">
-          <div className="row y-gap-20 justify-between items-end">
-            <div className="col-auto">
-              <div className="sectionTitle -md">
-                <h2 className="sectionTitle__title">Recommended Hotels</h2>
-                <p className=" sectionTitle__text mt-5 sm:mt-0">
-                  Interdum et malesuada fames ac ante ipsum
-                </p>
-              </div>
-            </div>
-            {/* End .col */}
-
-            <div className="col-auto">
-              <Link
-                href="#"
-                className="button -md -blue-1 bg-blue-1-05 text-blue-1"
-              >
-                More <div className="icon-arrow-top-right ml-15" />
-              </Link>
-            </div>
-          </div>
-          {/* End .row */}
-            <div className="col-auto">
-              <Link
-                href="#"
-                className="button -md -blue-1 bg-blue-1-05 text-blue-1"
-              >
-                More <div className="icon-arrow-top-right ml-15" />
-              </Link>
-            </div>
-            {/* End .col */}
-          </div>
-          {/* End .row */}
-
-          <div className="row y-gap-30 pt-40 sm:pt-20 item_gap-x30">
-            <Tours />
-          </div>
-          {/* End .row */}
-        </div>
-        {/* End .container */}
-      </section>
-      {/* End Tours Sections */}
-
+      {/* Top Sights */}
       <section className="layout-pt-md layout-pb-lg">
         <div className="container">
           <div className="row">
             <div className="col-auto">
               <div className="sectionTitle -md">
                 <h2 className="sectionTitle__title">Top sights in London</h2>
-                <p className=" sectionTitle__text mt-5 sm:mt-0">
+                <p className="sectionTitle__text mt-5 sm:mt-0">
                   These popular destinations have a lot to offer
                 </p>
               </div>
             </div>
           </div>
-          {/* End .row */}
 
           <div className="row y-gap-30 pt-40">
             <Slights />
           </div>
-          {/* End .row */}
 
           <div className="row justify-center mt-40">
             <div className="col-auto">
@@ -159,20 +104,16 @@ const Destinations = () => {
               </Link>
             </div>
           </div>
-          {/* End .row */}
         </div>
-        {/* End .container */}
       </section>
-      {/* End Top sights in London */}
 
+      {/* Testimonials */}
       <section className="layout-pt-lg layout-pb-lg bg-light-2">
         <div className="container">
           <div className="row y-gap-40 justify-between">
             <div className="col-xl-5 col-lg-6" data-aos="fade-up">
               <TestimonialLeftCol />
             </div>
-            {/* End col */}
-
             <div className="col-lg-6">
               <div
                 className="overflow-hidden js-testimonials-slider-3"
@@ -183,12 +124,10 @@ const Destinations = () => {
               </div>
             </div>
           </div>
-          {/* End .row */}
         </div>
-        {/* End container */}
       </section>
-      {/* End testimonial Section */}
 
+      {/* FAQs */}
       <section className="layout-pt-lg layout-pb-md">
         <div className="container">
           <div className="row y-gap-20">
@@ -199,21 +138,16 @@ const Destinations = () => {
                 Kashmir
               </h2>
             </div>
-            {/* End .col */}
-
             <div className="col-lg-8">
               <div className="accordion -simple row y-gap-20 js-accordion">
                 <Faq />
               </div>
             </div>
-            {/* End .col-lg-8 */}
           </div>
-          {/* End .row */}
         </div>
-        {/* End .container */}
       </section>
-      {/* End Faq Section */}
 
+      {/* Nearby Destinations */}
       <section className="layout-pt-md layout-pb-lg">
         <div className="container">
           <div className="row y-gap-20">
@@ -222,27 +156,24 @@ const Destinations = () => {
                 <h2 className="sectionTitle__title">
                   Destinations near Kashmir
                 </h2>
-                <p className=" sectionTitle__text mt-5 sm:mt-0">
+                <p className="sectionTitle__text mt-5 sm:mt-0">
                   These popular destinations have a lot to offer
                 </p>
               </div>
             </div>
           </div>
-          {/* End .row */}
 
           <div className="pt-40 relative">
             <TopDestinations2 />
           </div>
         </div>
-        {/* End .container */}
       </section>
-      {/* End top destinations */}
 
+      {/* Call to Actions */}
       <CallToActions />
-      {/* End Call To Actions Section */}
 
+      {/* Footer */}
       <DefaultFooter />
-      {/* End Call To Actions Section */}
     </>
   );
 };
