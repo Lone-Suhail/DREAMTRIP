44b1f4fe07c9a73dd31b18dae85373ef8ba4e7b8
Lone-Suhail <lonesuhail02@gmail.com>
2025-02-09 13:29:20 +0530

    Update page.jsx

--- gotrip/app/(tour)/tour-list-v3/page.jsx
+++ gotrip/app/(tour)/tour-list-v3/page.jsx
@@ -1,14 +1,15 @@
+"use client";
+
 import Header11 from "@/components/header/header-11";
 import DropdownSelelctBar from "@/components/tour-list/common/DropdownSelelctBar";
 import MapPropertyFinder from "@/components/tour-list/common/MapPropertyFinder";
 import Pagination from "@/components/tour-list/common/Pagination";
 import MainFilterSearchBox from "@/components/tour-list/tour-list-v3/MainFilterSearchBox";
 import TopHeaderFilter from "@/components/tour-list/tour-list-v3/TopHeaderFilter";
-import TourProperties from "@/components/tour-list/tour-list-v3/TourProperties";
 
 export const metadata = {
-  title: "Tour List v3 || Dream Trip - Travel & Tour React NextJS Template",
-  description: "Dream Trip - Travel & Tour React NextJS Template",
+  title: "Tour List v3 || Dream Trip - Travel & Tour",
+  description: "Dream Trip - Travel & Tour",
 };
 
 const index = () => {
