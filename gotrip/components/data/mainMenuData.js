d4e9630221d1bbc79b582197c2f03de7e2971cce
Lone-Suhail <lonesuhail02@gmail.com>
2025-02-11 00:28:34 +0530

    Update mainMenuData.js

--- gotrip/components/data/mainMenuData.js
+++ gotrip/components/data/mainMenuData.js
@@ -1,82 +1,40 @@
 export const homeItems = [
-  {
-    name: "Home",
-    routePath: "/",
-  },
+  { name: "Home", routePath: "/" },
 ];
+
 export const pageItems = [
-  {
-    name: "404",
-    routePath: "/404",
-  },
-  {
-    name: "About",
-    routePath: "/about",
-  },
-  {
-    name: "Become Expert",
-    routePath: "/become-expert",
-  },
-  {
-    name: "Help Center",
-    routePath: "/help-center",
-  },
-  {
-    name: "Login",
-    routePath: "/login",
-  },
-  {
-    name: "Register",
-    routePath: "/signup",
-  },
-  {
-    name: "Terms",
-    routePath: "/terms",
-  },
-  {
-    name: "Invoice",
-    routePath: "/invoice",
-  },
+  { name: "404", routePath: "/404" },
+  { name: "About", routePath: "/about" },
+  { name: "Become Expert", routePath: "/become-expert" },
+  { name: "Help Center", routePath: "/help-center" },
+  { name: "Login", routePath: "/login" },
+  { name: "Register", routePath: "/signup" },
+  { name: "Terms", routePath: "/terms" },
+  { name: "Invoice", routePath: "/invoice" },
 ];
 
 export const categorieMegaMenuItems = [
   {
-   
     id: 2,
     menuCol: [
       {
-        
         megaBanner: "/img/backgrounds/8.png",
         title: "Things to do on your tour",
         btnText: "See Tour",
         btnRoute: "/tour-list-v1",
         menuItems: [
           {
-           
             title: "Tour List",
             menuList: [
-              {
-                name: "Tour List v1",
-                routePath: "/tour-list-v1",
-              },
-              {
-                name: "Tour List v2",
-                routePath: "/tour-list-v2",
-              },
+              { name: "Tour List v1", routePath: "/tour-list-v1" },
+              { name: "Tour List v2", routePath: "/tour-list-v2" },
             ],
           },
           {
-            id: 2,
             title: "Tour Pages",
             menuList: [
-              {
-                name: "Tour Map",
-                routePath: "/tour-list-v3",
-              },
-              {
-                name: "Tour Single",
-                routePath: "/tour-single/5",
-              },
+              { name: "Tour Map", routePath: "/tour-list-v3" },
+              { name: "Tour Single", routePath: "/tour-single/5" },
             ],
           },
         ],
@@ -91,31 +49,17 @@ export const categorieMobileItems = [
     title: "Tour",
     menuItems: [
       {
-      
         title: "Tour List",
         menuList: [
-          {
-            name: "Tour List v1",
-            routePath: "/tour-list-v1",
-          },
-          {
-            name: "Tour List v2",
-            routePath: "/tour-list-v2",
-          },
+          { name: "Tour List v1", routePath: "/tour-list-v1" },
+          { name: "Tour List v2", routePath: "/tour-list-v2" },
         ],
       },
       {
-        id: 2,
         title: "Tour Pages",
         menuList: [
-          {
-            name: "Tour Map",
-            routePath: "/tour-list-v3",
-          },
-          {
-            name: "Tour Single",
-            routePath: "/tour-single/5",
-          },
+          { name: "Tour Map", routePath: "/tour-list-v3" },
+          { name: "Tour Single", routePath: "/tour-single/5" },
         ],
       },
     ],
