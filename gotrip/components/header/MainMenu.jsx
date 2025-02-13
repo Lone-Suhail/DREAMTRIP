273b72e3918ebb4f75e94184188bff218f1e3e60
Lone-Suhail <lonesuhail02@gmail.com>
2025-02-13 23:03:56 +0530

    Update MainMenu.jsx

--- gotrip/components/header/MainMenu.jsx
+++ gotrip/components/header/MainMenu.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { homeItems } from "../../data/mainMenuData"; // Removed pageItems
+import { homeItems } from "../../data/mainMenuData"; // Assuming homeItems contains the default home route
 import CategoriesMegaMenu from "./CategoriesMegaMenu";
-import { isActiveParentChaild, isActiveLink } from "../../utils/linkActiveChecker";
+import { isActiveLink } from "../../utils/linkActiveChecker";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 
@@ -9,26 +9,23 @@ const MainMenu = ({ style = "" }) => {
   const pathname = usePathname();
   const [isActiveParent, setIsActiveParent] = useState(false);
 
+  // Find the default home route (e.g., the first item in homeItems)
+  const defaultHomeRoute = homeItems[0]?.routePath || "/";
+
   return (
-    <nav className="menu js-navList">
+    <nav className="menu js-navList" role="navigation">
       <ul className={`menu__nav ${style} -is-active`}>
-        <li className={`${isActiveParentChaild(homeItems, pathname) ? "current" : ""} menu-item-has-children`}>
-          <a href="#">
+        {/* Home Menu (without dropdown) */}
+        <li className={isActiveLink(defaultHomeRoute, pathname) ? "current" : ""}>
+          <Link href={defaultHomeRoute}>
             <span className="mr-10">Home</span>
-            <i className="icon icon-chevron-sm-down" />
-          </a>
-          <ul className="subnav">
-            {homeItems.map((menu, i) => (
-              <li key={i} className={isActiveLink(menu.routePath, pathname) ? "current" : ""}>
-                <Link href={menu.routePath}>{menu.name}</Link>
-              </li>
-            ))}
-          </ul>
+          </Link>
         </li>
         {/* End home page menu */}
 
-        <li className={isActiveParent ? "menu-item-has-children -has-mega-menu current" : "menu-item-has-children -has-mega-menu"}>
-          <a href="#">
+        {/* Categories Mega Menu */}
+        <li className={`menu-item-has-children -has-mega-menu ${isActiveParent ? "current" : ""}`}>
+          <a href="#" aria-haspopup="true" aria-expanded={isActiveParent}>
             <span className="mr-10">Categories</span>
             <i className="icon icon-chevron-sm-down" />
           </a>
@@ -38,11 +35,13 @@ const MainMenu = ({ style = "" }) => {
         </li>
         {/* End categories menu */}
 
+        {/* Destinations Menu */}
         <li className={pathname === "/destinations" ? "current" : ""}>
           <Link href="/destinations">Destinations</Link>
         </li>
         {/* End destinations menu */}
 
+        {/* Contact Menu */}
         <li className={pathname === "/contact" ? "current" : ""}>
           <Link href="/contact">Contact</Link>
         </li>
